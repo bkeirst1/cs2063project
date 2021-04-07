a0b4794018a6c7a1b7b35e69af44e0eec462a903
bkeirst1 <bkeirst1@unb.ca>
2021-04-07 13:03:43 -0300

    implemented the compare feature

--- app/src/main/java/ca/unb/mobiledev/kimura/CompareActivity.java
+++ app/src/main/java/ca/unb/mobiledev/kimura/CompareActivity.java
@@ -1,12 +1,21 @@
 package ca.unb.mobiledev.kimura;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CompareActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class CompareActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +23,65 @@ public class CompareActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.compare_activity);
-
+        Spinner fighterOneSpinner = (Spinner)findViewById(R.id.fighterOneSpinner);
+        Spinner fighterTwoSpinner = (Spinner)findViewById(R.id.fighterTwoSpinner);
+        fighterOneSpinner.setOnItemSelectedListener(this);
+        fighterTwoSpinner.setOnItemSelectedListener(this);
+        ArrayList<Fighter> fighters = Data.getInstance().getFighters();
+        FighterAdapter adapter = new FighterAdapter(this, android.R.layout.simple_spinner_item, R.id.textView, fighters);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fighterOneSpinner.setAdapter(adapter);
+        fighterTwoSpinner.setAdapter(adapter);
         menuHandler();
     }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Fighter fighter = (Fighter)parent.getItemAtPosition(position);
+        TextView firstNameView;
+        TextView lastNameView;
+        TextView nickNameView;
+        TextView heightView;
+        TextView weightView;
+        TextView reachView;
+        TextView stanceView;
+        TextView recordView;
+        if(parent.getId() == R.id.fighterOneSpinner) {
+            System.out.println("one");
+            System.out.println(fighter.getLastName());
+            firstNameView = (TextView)findViewById(R.id.fighter_one_firstName);
+            lastNameView = (TextView)findViewById(R.id.fighter_one_lastName);
+            nickNameView = (TextView)findViewById(R.id.fighter_one_nickName);
+            heightView = (TextView)findViewById(R.id.fighter_one_height);
+            weightView = (TextView)findViewById(R.id.fighter_one_weight);
+            reachView = (TextView)findViewById(R.id.fighter_one_reach);
+            stanceView = (TextView)findViewById(R.id.fighter_one_stance);
+            recordView = (TextView)findViewById(R.id.fighter_one_record);
+        }
+        else {
+            System.out.println("two");
+            System.out.println(fighter.getLastName());
+            firstNameView = (TextView)findViewById(R.id.fighter_two_firstName);
+            lastNameView = (TextView)findViewById(R.id.fighter_two_lastName);
+            nickNameView = (TextView)findViewById(R.id.fighter_two_nickName);
+            heightView = (TextView)findViewById(R.id.fighter_two_height);
+            weightView = (TextView)findViewById(R.id.fighter_two_weight);
+            reachView = (TextView)findViewById(R.id.fighter_two_reach);
+            stanceView = (TextView)findViewById(R.id.fighter_two_stance);
+            recordView = (TextView)findViewById(R.id.fighter_two_record);
+        }
+        firstNameView.setText(fighter.getFirstName());
+        lastNameView.setText(fighter.getLastName());
+        nickNameView.setText(fighter.getNickName());
+        heightView.setText(fighter.getHeight());
+        weightView.setText(fighter.getWeight() + "");
+        reachView.setText(fighter.getReach() + "");
+        stanceView.setText(fighter.getStance());
+        recordView.setText(fighter.getRecord()[0] + "-" + fighter.getRecord()[1] + "-" + fighter.getRecord()[2]);
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {}
 
     private void menuHandler() {
         Button rosterButton;
---
+++ app/src/main/java/ca/unb/mobiledev/kimura/FighterAdapter.java
@@ -0,0 +1,47 @@
+package ca.unb.mobiledev.kimura;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class FighterAdapter extends ArrayAdapter<Fighter> {
+
+    private final Activity context;
+
+    public FighterAdapter(Activity context, int resourceID, int textViewID, ArrayList<Fighter> list){
+        super(context, resourceID, textViewID, list);
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return rowView(convertView, position);
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        return rowView(convertView, position);
+    }
+
+    private View rowView(View convertView , int position){
+        Fighter item = getItem(position);
+        LayoutInflater inflater = context.getLayoutInflater();
+        @SuppressLint({"InflateParams", "ViewHolder"})
+        View rowView = inflater.inflate(R.layout.fighter_row, null,true);
+        TextView field = (TextView)rowView.findViewById(R.id.textView);
+        field.setText(item.getFirstName() + " " + item.getLastName());
+        return rowView;
+    }
+
+    private class viewHolder {
+        TextView txtTitle;
+        ImageView imageView;
+    }
+}
